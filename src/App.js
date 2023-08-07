438d3518667bf395f238e3e6ea52169261008ba3
junyeokk <cjh4302@gmail.com>
2023-08-07 23:26:58 +0900

    feat: donor/home 생성

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
 
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
+import LogoPage from "./pages/LogoPage";
+import TestPage from "./pages/TestPage";
+import Home from "./pages/donor/Home";
 
-import LogoPage from './pages/LogoPage';
-import TestPage from './pages/TestPage';
 function App() {
   return (
     <div className="App">
       {/* 테스트 문구-현재 어플에 이 문구 뜸 */}
       Ditto Frontend start
-    
-    {/* 라우터 주소는 협의 후 결정하는걸로 */}
- <Routes>
-  <Route path='/logo' element={<LogoPage></LogoPage>}></Route>
-  <Route path='/test' element={<TestPage></TestPage>}></Route>
- </Routes>
+      {/* 라우터 주소는 협의 후 결정하는걸로 */}
+      <Routes>
+        <Route path="/logo" element={<LogoPage></LogoPage>}></Route>
+        <Route path="/test" element={<TestPage></TestPage>}></Route>
+        <Route path="/donor/home" element={<Home></Home>}></Route>
+      </Routes>
     </div>
   );
 }
