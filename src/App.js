a77922b91a735bd65b32f5d2ea488083ebf6207b
junyeokk <cjh4302@gmail.com>
2023-08-11 23:31:49 +0900

    feat: /donor/mypage (평점, 봉사 횟수, 포인터, 1365)

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 
-
-
 // 공통
 import Logo from "./pages/Logo";
 import Selection from "./pages/Selection";
@@ -10,6 +8,9 @@ import Selection from "./pages/Selection";
 // 도움 주는 사람
 import Home from "./pages/donor/Home";
 import Login from "./pages/donor/Login";
+import MyPage from "./pages/donor/MyPage";
+
+// 도움 받는 사람
 import HelpB from "./pages/asker/HelpB";
 import HelpS from "./pages/asker/HelpS";
 
@@ -18,8 +19,6 @@ function App() {
     <div className="App">
       {/* 라우터 주소는 협의 후 결정하는걸로 */}
       <Routes>
-        
-
         {/* 공통 */}
         <Route path="/" element={<Logo />}></Route>
         <Route path="/selection" element={<Selection />}></Route>
@@ -27,10 +26,11 @@ function App() {
         {/* 도움 주는 사람 */}
         <Route path="/donor/login" element={<Login />}></Route>
         <Route path="/donor/home" element={<Home />}></Route>
+        <Route path="/donor/mypage" element={<MyPage />}></Route>
 
         {/* 도움 받는사람 */}
-        <Route path="/asker/helpB" element={<HelpB/>}></Route>
-        <Route path="/asker/helpS" element={<HelpS/>}></Route>
+        <Route path="/asker/helpB" element={<HelpB />}></Route>
+        <Route path="/asker/helpS" element={<HelpS />}></Route>
       </Routes>
     </div>
   );
