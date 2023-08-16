8603fe4182b7d950e8cfddf6da77a551ff6869b8
junyeokk <cjh4302@gmail.com>
2023-08-16 16:27:56 +0900

    feat: /donor/login logo

--- src/pages/donor/Login.js
+++ src/pages/donor/Login.js
@@ -1,13 +1,17 @@
 import { Link } from "react-router-dom";
-import iconImg from "../../assets/icon.png";
+import iconImg from "../../assets/logo.png";
 
 const Login = () => {
   return (
     <>
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <img className="mx-auto w-[100px]" src={iconImg} alt="Ditto" />
-          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+          <img
+            className="mx-auto w-[100px] h-[90px]"
+            src={iconImg}
+            alt="Ditto"
+          />
+          <h2 className="mt-5 text-center text-3xl font-bold leading-9 tracking-tight text-gray-900">
             Ditto
           </h2>
         </div>
