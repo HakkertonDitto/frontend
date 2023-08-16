97e4a3d6416c466132278513c18828053a857f46
junyeokk <cjh4302@gmail.com>
2023-08-16 16:17:30 +0900

    feat: login page design

--- src/pages/donor/Login.js
+++ src/pages/donor/Login.js
@@ -1,50 +1,80 @@
 import { Link } from "react-router-dom";
+import iconImg from "../../assets/icon.png";
 
 const Login = () => {
   return (
     <>
-      <div className="flex justify-center">
-        <div className="w-[360px] h-[800px] relative bg-white">
-          <div className="w-[307px] left-[26px] top-[397px] absolute text-center text-neutral-500 text-sm font-normal leading-tight">
-            회원가입 / 비밀번호찾기
-          </div>
-          <div className="w-[272px] left-[45px] top-[81px] absolute text-center text-neutral-950 text-[28px] font-bold leading-9">
-            환영합니다!
-          </div>
-          <Link to="/donor/home">
-            <div className="w-[327px] h-12 py-4 left-[17px] top-[339px] absolute bg-emerald-400 rounded-[48px] justify-center items-center inline-flex">
-              <div className="text-center text-white text-base font-bold leading-none">
+      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <img className="mx-auto w-[100px]" src={iconImg} alt="Ditto" />
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+            Ditto
+          </h2>
+        </div>
+
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form className="space-y-6" action="/donor/home" method="GET">
+            <div>
+              <label
+                htmlFor="id"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                아이디
+              </label>
+              <div className="mt-2">
+                <input
+                  id="id"
+                  name="id"
+                  type="id"
+                  autoComplete="id"
+                  required
+                  className="block w-full rounded-md border-0 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 p-3"
+                />
+              </div>
+            </div>
+
+            <div>
+              <div className="flex items-center justify-between">
+                <label
+                  htmlFor="password"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  비밀번호
+                </label>
+                <div className="text-sm">
+                  <a href="#" className="font-semibold text-[#2CBB99]">
+                    비밀번호를 잊으셨나요?{" "}
+                  </a>
+                </div>
+              </div>
+              <div className="mt-2">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  required
+                  className="block w-full rounded-md border-0 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#2CBB99] sm:text-sm sm:leading-6 p-3"
+                />
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-full bg-[#2CBB99] p-2 text-sm font-semibold leading-6 text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 "
+              >
                 로그인
-              </div>
+              </button>
             </div>
-          </Link>
-          <div className="w-[375px] h-12 pl-[18px] pr-[333px] py-3 left-[-7px] top-[10px] absolute bg-white justify-start items-center inline-flex">
-            <div className="w-6 h-6 relative flex-col justify-start items-start flex" />
-          </div>
-          <div className="w-[350px] h-[68px] left-[27px] top-[152px] absolute">
-            <div className="h-12 pl-4 pr-[180px] py-4 left-0 top-[20px] absolute bg-white rounded-lg border border-zinc-200 justify-start items-center inline-flex">
-              <div className="self-stretch flex-col justify-start items-start gap-1 inline-flex">
-                <div className="self-stretch h-4 text-zinc-400 text-xs font-normal leading-none">
-                  아이디를 입력해주세요.
-                </div>
-              </div>
-            </div>
-            <div className="w-[40px] h-4 left-0 top-0 absolute text-neutral-950 text-xs font-normal leading-none">
-              아이디
-            </div>
-          </div>
-          <div className="w-[350px] h-[68px] left-[26px] top-[244px] absolute">
-            <div className="h-12 pl-4 pr-[169px] py-4 left-0 top-[20px] absolute bg-white rounded-lg border border-zinc-200 justify-start items-center inline-flex">
-              <div className="self-stretch flex-col justify-start items-start gap-1 inline-flex">
-                <div className="self-stretch h-4 text-zinc-400 text-xs font-normal leading-none">
-                  비밀번호를 입력해주세요.
-                </div>
-              </div>
-            </div>
-            <div className="w-[50px] h-4 left-0 top-0 absolute text-neutral-950 text-xs font-normal leading-none">
-              비밀번호
-            </div>
-          </div>
+          </form>
+
+          <p className="mt-10 text-center text-sm text-gray-500">
+            회원이 아니신가요?{" "}
+            <a href="#" className="font-semibold leading-6 text-[#2CBB99]">
+              회원가입하기
+            </a>
+          </p>
         </div>
       </div>
     </>
